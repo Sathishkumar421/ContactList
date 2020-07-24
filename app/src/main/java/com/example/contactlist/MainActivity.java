e523c0afa12cf0810257e143000937925e121268
Sathishkumar421 <sathish199421@gmail.com>
2020-07-24 09:51:15 +0530

    RecyclerVIew

--- app/src/main/java/com/example/contactlist/MainActivity.java
+++ app/src/main/java/com/example/contactlist/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.contactlist;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -9,15 +12,18 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.contactlist.Data.DatabaseHandler;
+import com.example.contactlist.adapter.RecyclerViewAdapter;
 import com.example.contactlist.model.contact;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private ListView listView;
-    private ArrayList<String> contactArrayList;
-    private ArrayAdapter<String> arrayAdapter;
+  //  private ListView listView;
+      private RecyclerView recyclerView;
+      private RecyclerViewAdapter recyclerViewAdapter;
+      private ArrayList<contact> contactArrayList;
+  //  private ArrayAdapter<String> arrayAdapter;
     private int count;
 
 
@@ -25,41 +31,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = findViewById(R.id.listview);
+      //  listView = findViewById(R.id.listview);
+          recyclerView=findViewById( R.id.RecyclerView);
+          recyclerView.setHasFixedSize(true);
+          recyclerView.setLayoutManager(new LinearLayoutManager(this));
         contactArrayList = new ArrayList<>();
         DatabaseHandler db = new DatabaseHandler(MainActivity.this);
 
-          db.addContact(new contact("ABC","6131464332"));
-          db.addContact(new contact("LMN","0945458765"));
-          db.addContact(new contact("Rajesh","9698639468"));
-          db.addContact(new contact("Dinesh","7200232567"));
-          db.addContact(new contact("Lokesh","9790415413"));
+        db.addContact(new contact("ABC","6131464332"));
+        db.addContact(new contact("LMN","0945458765"));
+        db.addContact(new contact("Rajesh","9698639468"));
+        db.addContact(new contact("Dinesh","7200232567"));
+        db.addContact(new contact("Lokesh","9790415413"));
 
         List<contact> contactList = db.getAllContacts();
 
         for (contact contact : contactList) {
             Log.d("MainActivity", "onCreate: " + contact.getName());
-            contactArrayList.add(contact.getName());
+            contactArrayList.add(contact);
         }
 
         //create array adapter
-        arrayAdapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                contactArrayList
-        );
-
-        listView.setAdapter(arrayAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.d("List", "onItemClick: " + contactArrayList.get(position));
-
-
-
-            }
-        });
+        recyclerViewAdapter =new RecyclerViewAdapter(MainActivity.this,contactList);
+         recyclerView.setAdapter(recyclerViewAdapter);
 
     }
 
---
+++ app/src/main/java/com/example/contactlist/adapter/RecyclerViewAdapter.java
@@ -0,0 +1,61 @@
+package com.example.contactlist.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.contactlist.R;
+import com.example.contactlist.model.contact;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+        private Context context;
+        private List<contact> ContactList;
+
+    public RecyclerViewAdapter(Context context, List<contact> contactList) {
+        this.context = context;
+        this.ContactList = contactList;
+    }
+
+    @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view= LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.single_contact,parent,false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+            contact contact= ContactList.get(position);
+            holder.PhoneNumber.setText(contact.getPhoneNumber());
+            holder.contactName.setText(contact.getName());
+
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return ContactList.size() ;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView contactName;
+            public TextView PhoneNumber;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+
+                contactName= itemView.findViewById(R.id.Name);
+                PhoneNumber= itemView.findViewById(R.id.Phone_Number);
+            }
+        }
+    }
